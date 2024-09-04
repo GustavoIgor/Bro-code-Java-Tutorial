87325ea292caa6042b31f536f33a8d135aea8495
Gustavo Boer <Gustavo.igorsb@outlook.com>
2024-09-04 05:37:38 -0300

    Primeira hora do curso realizada, um bug encontrado na biblioteca JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //---Interface gráfica--- <-- *POR ALGUMO MOTIVO NÃO FUNCIONA NO MEIO DO CÓDIGO*
+        String name = JOptionPane.showInputDialog("Insira seu nome: ");
+        JOptionPane.showMessageDialog(null, "Olá, " + name + ".");
+        int idade2 = Integer.parseInt(JOptionPane.showInputDialog("Insira sua idade: "));
+        JOptionPane.showMessageDialog(null, "Você tem " + idade2 + " anos.");
+        double altura = Double.parseDouble(JOptionPane.showInputDialog("Insira sua altura: "));
+        JOptionPane.showMessageDialog(null, "Você tem " + altura + "cm.");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //---Trocar duas variáveis---
+        String x = "Água";
+        String y = "Refresco";
+        String temp;
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+
+        //---Input e output---
+        String nome;
+        int idade;
+        String comida;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Qual é o teu nome?");
+        nome = scanner.nextLine();
+        System.out.println("Qual a tua idade?");
+        idade = scanner.nextInt();
+        //Esta linha existe somente para limpar o scanner do espaço que não foi lido.
+        scanner.nextLine();
+        System.out.println("Qual a tua comida favorita?");
+        comida = scanner.nextLine();
+
+        System.out.println("Seu nome é: " + nome +
+                "\nSua idade é: " + idade +
+                "\nSua comida favorita é: " + comida);
     }
 }
