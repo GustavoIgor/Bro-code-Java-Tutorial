85566f4c61170f0529258b94993ea0b566de6092
Gustavo Boer <Gustavo.igorsb@outlook.com>
2024-09-07 17:18:55 -0300

    Segunda hora do curso completa.

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int idade;
         double altura;
-        int escolha = 0;
+        int escolha;
         String x = "Água";
         String y = "Refresco";
         String temp;
-        String nome = "";
+        String nome;
         String comida;
         double catop;
         double catad;
@@ -25,10 +25,11 @@ public class Main {
         String simbolo;
         String[] carros = {"Corvette", "Tesla", "Camaro"};
         String[][] carros2 = new String[3][3];
+        boolean bollean;
 
         //---Interface gráfica--- <-- *POR ALGUMO MOTIVO NÃO FUNCIONA NO MEIO DO CÓDIGO*
-        String name = JOptionPane.showInputDialog("Insira seu nome: ");
-        JOptionPane.showMessageDialog(null, "Olá, " + name + ".");
+        nome = JOptionPane.showInputDialog("Insira seu nome: ");
+        JOptionPane.showMessageDialog(null, "Olá, " + nome + ".");
         idade = Integer.parseInt(JOptionPane.showInputDialog("Insira sua idade: "));
         JOptionPane.showMessageDialog(null, "Você tem " + idade + " anos.");
         altura = Double.parseDouble(JOptionPane.showInputDialog("Insira sua altura: "));
@@ -175,8 +176,88 @@ public class Main {
                 break;
             case 10:
                 //Matrizes / array com duas camadas
+                carros2[0][0] = "Camaro";
+                carros2[0][1] = "Corvette";
+                carros2[0][2] = "Silverado";
+                carros2[1][0] = "Mustang";
+                carros2[1][1] = "Ranger";
+                carros2[1][2] = "F-150";
+                carros2[2][0] = "Ferrari";
+                carros2[2][1] = "Lamborghinni";
+                carros2[2][2] = "Tesla";
 
+                for (i = 0; i < carros2.length; i++){
+                    System.out.println();
+                    for (j = 0; j < carros2[i].length; j++){
+                        System.out.print(carros2[i][j] + " ");
+                    }
+                }
                 break;
+
+            case 11:
+                //Métodos da String
+                System.out.println("Insira uma frase qualquer: ");
+                x = scanner.nextLine();
+                System.out.println("Escolha um número: ");
+                escolha = scanner.nextInt();
+                scanner.nextLine();
+                switch (escolha){
+                    case 1:
+                        //equals
+                        System.out.println("Insira outra frase igual ou diferente: ");
+                        y = scanner.nextLine();
+                        if (x.equalsIgnoreCase(y)){
+                            System.out.println("As frases são iguais!");
+                        } else {
+                            System.out.println("As frases NÃO são iguais!");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Esta é a quantidade de caracteres da frase:" + x.length());
+                        break;
+                    case 3:
+                        System.out.println("Insira qual o caractere que você quer ver separadamente: ");
+                        escolha = scanner.nextInt();
+                        System.out.println("O caractere na posição de número " + escolha + " é o: " + x.charAt(escolha));
+                        break;
+                    case 4:
+                        System.out.println("Insira qual o caractere que você quer ver separadamente: ");
+                        y = scanner.nextLine();
+                        if (!x.contains(y)){
+                            System.out.println("Não existe este caractere na String!");
+                        }else{
+                            System.out.println("O caractere " + y + " está na posição: " + x.indexOf(y));
+                        }
+                        break;
+                    case 5:
+                        if (x.isEmpty()){
+                            System.out.println("Você não inseriu nenhuma frase!");
+                        } else {
+                            System.out.println("Você inseriu a frase: " + x + ".");
+                        }
+                        break;
+                    case 6:
+                        System.out.println("Esta é a frase com todos os caracteres em maiúsculas: " + x.toUpperCase() + ".");
+                        break;
+                    case 7:
+                        System.out.println("Esta é a frase com todos os caracteres em minúsculas: " + x.toLowerCase() + ".");
+                        break;
+                    case 8:
+                        System.out.println("Está é a frase sem espaços no inicio e no fim: " + x.trim() + ".");
+                        break;
+                    case 9:
+                        System.out.println("Insira o caractere que quer trocar: ");
+                        y = scanner.nextLine();
+                        System.out.println("Insira o caractere para ser colocado no lugar: ");
+                        temp = scanner.nextLine();
+                        System.out.println("A frase com o caractere substituido é: " + x.replace(y, temp) + ".");
+                        break;
+                    default:
+                        System.out.println("Você inseriu um número inválido!");
+                }
+                break;
+            case 12:
+
             default:
                 System.out.println("Você inseriu um número que não existe!");
             break;
